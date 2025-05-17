b4816d0a81852fa8ac0b11182b0d6fb483f73fca
Geries Handal <gghandal@gmail.com>
2025-05-17 07:44:03 +0200

    fix: disable vercel analytics

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -6,7 +6,7 @@ import {
   Scripts,
 } from '@tanstack/react-router';
 import { TanStackRouterDevtools } from '@tanstack/react-router-devtools';
-import { Analytics } from '@vercel/analytics/react';
+//import { Analytics } from '@vercel/analytics/react';
 
 import appCss from '~/styles/app.css?url';
 import { DefaultCatchBoundary } from '~/components/DefaultCatchBoundry';
@@ -67,7 +67,7 @@ export const Route = createRootRoute({
 function RootComponent() {
   return (
     <RootDocument>
-      <Analytics />
+      {/*      <Analytics />*/}
       <Outlet />
     </RootDocument>
   );
