73f5461e6cc09386eaed6a292f74b6a12fb1793a
Geries Handal <gghandal@gmail.com>
2026-01-01 21:57:56 +0100

    fix: actions and error

--- src/routes/_pathlessLayout/item/$id.tsx
+++ src/routes/_pathlessLayout/item/$id.tsx
@@ -3,8 +3,17 @@ import { getComments } from '~/features/hnstories/server-functions/getComments';
 import { HNStoryComments } from '~/features/hnstories/HNStoryComments';
 import { LoadingIndicator } from '~/components/ui/LoadingIndicator';
 import { Suspense } from 'react';
+import { z } from 'zod';
 
 export const Route = createFileRoute('/_pathlessLayout/item/$id')({
+  params: {
+    parse: (params) => ({
+      id: z.coerce.number().parse(params.id),
+    }),
+    stringify: (params) => ({
+      id: String(params.id),
+    }),
+  },
   component: RouteComponent,
   loader: async ({ params: { id } }) => {
     return { deferredComments: getComments({ data: id }) };
