53dd9c3220a56de3029393639ed1aed74afae7eb
Geries Handal <gghandal@gmail.com>
2026-02-11 09:36:42 +0100

    fix: disable typesense sync

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -2,16 +2,5 @@ import {
   saveTopStories,
   saveTopStoriesCron,
 } from './workflows/syncTopStories/saveTopStories';
-import {
-  initTypesenseCollections,
-  syncTypesenseCommentsDocuments,
-  syncTypesenseStoriesDocuments,
-} from '~/inngest/workflows/typesense/typesense';
 
-export const functions = [
-  saveTopStories,
-  saveTopStoriesCron,
-  initTypesenseCollections,
-  syncTypesenseStoriesDocuments,
-  syncTypesenseCommentsDocuments,
-];
+export const functions = [saveTopStories, saveTopStoriesCron];
