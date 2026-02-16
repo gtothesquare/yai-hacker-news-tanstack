1204764f18f67417d5e0a82d4cd42107b413f4c3
Geries Handal <gghandal@gmail.com>
2026-02-16 10:39:41 +0100

    fix: errors should not leak

--- src/components/DefaultCatchBoundry.tsx
+++ src/components/DefaultCatchBoundry.tsx
@@ -1,24 +1,15 @@
-import {
-  ErrorComponent,
-  Link,
-  rootRouteId,
-  useMatch,
-  useRouter,
-} from '@tanstack/react-router';
-import type { ErrorComponentProps } from '@tanstack/react-router';
+import { Link, rootRouteId, useMatch, useRouter } from '@tanstack/react-router';
 
-export function DefaultCatchBoundary({ error }: ErrorComponentProps) {
+export function DefaultCatchBoundary() {
   const router = useRouter();
   const isRoot = useMatch({
     strict: false,
     select: (state) => state.id === rootRouteId,
   });
 
-  console.error('DefaultCatchBoundary Error:', error);
-
   return (
     <div className="min-w-0 flex-1 p-4 flex flex-col items-center justify-center gap-6">
-      <ErrorComponent error={error} />
+      <h2>There was an issue, sorry it is our fault, fixing soon.</h2>
       <div className="flex gap-2 items-center flex-wrap">
         <button
           onClick={() => {
